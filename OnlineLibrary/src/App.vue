9840c098a9738cb6cab037ae9c2d8a1a872f7b80
MethulSilva-CS <methul_silva@protonmail.com>
2025-05-21 21:08:54 +0530

    imported category and detail components, added router-view for dynamic rendering

---
+++ OnlineLibrary/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import CategoryPage from './components/CategoryPage.vue'
+import ComputerPage from './components/ComputerPage.vue'
+import PhilosophyPage from './components/PhilosophyPage.vue'
+import RomancePage from './components/RomancePage.vue'
+import CB6Page from './components/CB6Page.vue'
+import PB6Page from './components/PB6Page.vue'
+import RB6Page from './components/RB6Page.vue'
+import LoginModal from './components/LoginModal.vue'
+</script>
+
+<template>
+ 
+<router-view />
+</template>
